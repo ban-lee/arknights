0666afe846a0b1a4616cf58ebc219fdb69a53451
Ban Lee <53801519+ban-lee@users.noreply.github.com>
2023-04-13 14:44:55 -0400

    Update avatars and fix url encoding

--- src/components/skins/skin.tsx
+++ src/components/skins/skin.tsx
@@ -61,7 +61,7 @@ export function Skin({ skin }: SkinProps) {
       >
         {imageAvailable && (
           <Image
-            src={`/avatars/${skin.imgId.replace('#', '-')}.png`}
+            src={`/avatars/${skin.imgId.replace('#', '%23')}.png`}
             alt={skin.name}
             width={120}
             height={120}
