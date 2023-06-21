e42abd77842ef4aca57ec082fb75043bb5513641
Ban Lee <53801519+ban-lee@users.noreply.github.com>
2023-06-21 17:03:02 -0400

    Fix font flashing on load in event components

--- src/hooks/useTopColour.ts
+++ src/hooks/useTopColour.ts
@@ -1,19 +1,21 @@
 import { isLight } from '@/utils/colour';
+import { MantineTheme, useMantineTheme } from '@mantine/core';
 import { useEffect, useState } from 'react';
-import { useMantineTheme } from '@mantine/core';
+
+function calculateFontColour(theme: MantineTheme, topColour: string): string {
+  if (!topColour) {
+    return theme.white;
+  }
+  return isLight(topColour) ? theme.black : theme.white;
+}
 
 export function useTopColour(topColour: string): [string, string] {
   const theme = useMantineTheme();
-  const [fontColour, setFontColour] = useState('');
+  const [fontColour, setFontColour] = useState(calculateFontColour(theme, topColour));
 
   useEffect(() => {
-    if (!topColour) {
-      setFontColour(theme.white);
-      return;
-    }
-
-    const isBgLight = isLight(topColour);
-    setFontColour(isBgLight ? theme.black : theme.white);
+    const colour = calculateFontColour(theme, topColour);
+    setFontColour(colour);
   }, [theme, topColour]);
 
   return [topColour, fontColour];
